f007603c88e74a34209e32fdb109189d5a0d2543
Abkreno <mohamedelhamamsy789@gmail.com>
2015-03-19 17:16:19 +0200

    Adding Methods Signatures and Exception Files

--- src/com/nan/DBApp.java
+++ src/com/nan/DBApp.java
@@ -1,5 +1,48 @@
 package com.nan;
 
-public class DBApp {
+import java.util.Hashtable;
+import java.util.Iterator;
 
+public class DBApp {
+	public void init() {
+
+	}
+
+	public void createTable(String strTableName,
+			Hashtable<String, String> htblColNameType,
+			Hashtable<String, String> htblColNameRefs, String strKeyColName)
+			throws DBAppException {
+
+	}
+
+	public void createIndex(String strTableName, String strColName)
+			throws DBAppException {
+
+	}
+
+	public void createMultiDimIndex(String strTableName,
+			Hashtable<String, String> htblColNames) throws DBAppException {
+
+	}
+
+	public void insertIntoTable(String strTableName,
+			Hashtable<String, String> htblColNameValue) throws DBAppException {
+
+	}
+
+	public void deleteFromTable(String strTableName,
+			Hashtable<String, String> htblColNameValue, String strOperator)
+			throws DBEngineException {
+
+	}
+
+	public Iterator selectFromTable(String strTable,
+			Hashtable<String, String> htblColNameValue, String strOperator)
+			throws DBEngineException {
+		return null;
+	}
+
+	public void saveAll() throws DBEngineException {
+
+	}
 }
---
+++ src/com/nan/DBAppException.java
@@ -0,0 +1,5 @@
+package com.nan;
+
+public class DBAppException extends Throwable {
+
+}
