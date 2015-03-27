2eff6c4944b212d58451adf0753946644a8010a5
kady <elkadyabdelrahman@gmail.com>
2015-03-27 21:53:03 +0200

    Testing primary key index

--- src/com/nan/commands/CommandsTest.java
+++ src/com/nan/commands/CommandsTest.java
@@ -4,14 +4,18 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
+import java.lang.reflect.Field;
 import java.util.Hashtable;
 
+import org.junit.After;
 import org.junit.Test;
 
 import com.nan.DBApp;
 import com.nan.DBAppException;
 import com.nan.DBEngineException;
 import com.nan.schema.Schema;
+import com.nan.schema.Table;
+import com.nan.schema.indices.DBLinearHashTable;
 import com.nan.utils.FileManager;
 
 public class CommandsTest {
@@ -53,8 +57,6 @@ public class CommandsTest {
 
 		Command createTableCommand = new CreateTableCommand(database,
 				tableName, colNameType, new Hashtable<String, String>(), "ID");
-
-		Schema.init();
 
 		assertFalse("Table folders shouldn't exist before creation",
 				tablePath.exists());
@@ -98,8 +100,6 @@ public class CommandsTest {
 		Command createTableCommand = new CreateTableCommand(database,
 				tableName, colNameType, new Hashtable<String, String>(), "ID");
 
-		Schema.init();
-
 		Switch.execute(createTableCommand);
 
 		FileManager.deleteFolder(tablesDirectory + "/" + tableName);
@@ -109,5 +109,56 @@ public class CommandsTest {
 		assertFalse("The table's folder shouldn't exist", new File(
 				tablesDirectory + "/" + tableName).exists());
 
+	}
+
+	@Test
+	public void testPrimaryIndex() throws DBAppException, DBEngineException {
+		DBApp database = new DBApp();
+
+		Hashtable<String, String> colNameType = new Hashtable<String, String>();
+
+		colNameType.put("Name", "java.lang.String");
+		colNameType.put("ID", "java.lang.Integer");
+
+		String tableName = "HelloWorld";
+
+		String userDirectory = System.getProperty("user.dir");
+		String tablesDirectory = userDirectory + "/data/tables/";
+
+		Command createTableCommand = new CreateTableCommand(database,
+				tableName, colNameType, new Hashtable<String, String>(), "ID");
+
+		Schema.init();
+
+		Switch.execute(createTableCommand);
+
+		Table table = new Table("HelloWorld");
+
+		try {
+			Field f = table.getClass().getDeclaredField("tableIndices");
+			f.setAccessible(true);
+
+			assertTrue("Index should be added to the table on primary key",
+					((Hashtable<String, DBLinearHashTable>) f.get(table))
+							.keySet().contains("ID"));
+
+		} catch (NoSuchFieldException | SecurityException e) {
+		} catch (IllegalArgumentException e) {
+
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+
+			e.printStackTrace();
+		}
+
+	}
+
+	@After
+	public void clearTablePath() {
+		String tableName = "HelloWorld";
+
+		String userDirectory = System.getProperty("user.dir");
+		String tablesDirectory = userDirectory + "/data/tables/";
+		FileManager.deleteFolder(tablesDirectory + "/" + tableName);
 	}
 }
