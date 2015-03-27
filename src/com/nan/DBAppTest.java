6f97699569e835026bb72747361a9d86ac581c56
Mohamed Mostafa <mohamedelhamamsy789@gmail.com>
2015-03-27 23:55:36 +0200

    Adds delete test

--- src/com/nan/DBAppTest.java
+++ src/com/nan/DBAppTest.java
@@ -108,4 +108,27 @@ public class DBAppTest {
 		t.insertIntoTable(htb);
 	}
 
+	@Test
+	public void testDelete() throws DBAppException {
+		Hashtable<String, String> htblColNameType = new Hashtable<String, String>();
+		htblColNameType.put("Name", "String");
+		htblColNameType.put("Age", "Integer");
+		htblColNameType.put("Mail", "Integer");
+
+		Hashtable<String, String> htblColNameRefs = new Hashtable<String, String>();
+		String strKeyColName = "Age";
+		Table t = new Table("Person", htblColNameType, htblColNameRefs,
+				strKeyColName);
+		Hashtable<String, String> htb = new Hashtable<String, String>();
+		htb.put("Name", "hey");
+		htb.put("Age", "11");
+		htb.put("Mail", "1@hotmail.com");
+		t.insertIntoTable(htb);
+		try {
+			t.deleteFromTable(htb, "And");
+		} catch (DBEngineException e) {
+			e.printStackTrace();
+		}
+	}
+
 }
